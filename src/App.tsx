c501b01b2e963e275e7ea1cc55bf9d1f2eccf566
Mark Gibson <mgibson@adaptavist.com>
2019-12-06 10:03:09 +0000

    Added lightning talk code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Observable, from, timer, zip } from "rxjs";
+import { repeat, map } from "rxjs/operators";
+
+
+// Some words...
+
+const words = "How to create a custom RxJS React Hook"
+
+
+// Some RxJS Observables...
+
+const tick$ = timer(1000, 500)
+
+const w$ = from(words.split(' '))
+
+const word$: Observable<string> = zip(w$, tick$).pipe(
+    map(([w]) => w),
+    repeat()
+)
+
+
+// A React Component...
 
 const App: React.FC = () => {
+
+  const word = 'Hello'
+
+  // const [word, update] = useState<string>('Hello')
+  
+  // useEffect(() => {
+  //   window.setTimeout(() => {
+  //     update('Welcome')
+  //   }, 3000)
+  // }, [])
+
+  // useEffect(() => {
+  //   word$.subscribe(update)
+  // }, [])
+
+  // const word = useStateObservable(word$, '...');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          {word}
+        </h1>
       </header>
     </div>
-  );
+  )
 }
+
+
+// Custom Hook...
+
+// function useStateObservable<T>(value$: Observable<T>, initValue: T): T {
+
+//   const [value, update] = useState<T>(initValue)
+
+//   useEffect(() => {
+//     const subscription = value$.subscribe(update)
+
+//     return () => {
+//       subscription.unsubscribe()
+//     }
+//   }, [value$])
+
+//   return value
+// }
+
 
 export default App;
